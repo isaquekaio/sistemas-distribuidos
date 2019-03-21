26784d53939d56ed728ff63e3fc7f506acebdc33
Isaque Kaio de Araujo Rodrigues <isaque.araujo@academico.ifrn.edu.br>
2019-03-21 15:47:42 -0300

    Inicado o projeto da disciplina sistemas-distribuidos

---
+++ cliente-servidor/distribuido/src/distribuido/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distribuido;
+
+/**
+ *
+ * @author 20172014040034
+ */
+public class Cliente {
+    
+}
