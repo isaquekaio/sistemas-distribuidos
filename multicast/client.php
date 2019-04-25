4d5e4032123cd84cb1054abc5c3080b287d622f1
Isaque Kaio <32231299+kaio7@users.noreply.github.com>
2019-04-25 16:21:16 -0300

    Adicionado o "	socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);"

--- multicast/client.php
+++ multicast/client.php
@@ -7,6 +7,7 @@
 	$grpparms = array("group" => "224.0.0.0", "interface" => "enp0s31f6");
 	$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 	socket_bind($sock,'0.0.0.0',$port);
+	socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
 	socket_set_option($sock,IPPROTO_IP,MCAST_JOIN_GROUP, $grpparms);
 	socket_recvfrom($sock,$recvStr,$len,$flags,$from,$port);
 	echo("Received " . $recvStr . " from " . $from); 
