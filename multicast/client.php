6a2a5043843c89468a604943b936491e28a8cae2
Isaque Kaio <32231299+kaio7@users.noreply.github.com>
2019-04-29 13:36:05 -0300

    Consertando o codigó com a ajuda de Israel

--- multicast/client.php
+++ multicast/client.php
@@ -6,11 +6,11 @@
 	$port = 14446;
 	$grpparms = array("group" => "224.0.0.0", "interface" => "enp0s31f6");
 	$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-	socket_bind($sock,'0.0.0.0',$port);
 	socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
+	socket_bind($sock,'0.0.0.0',$port);
 	socket_set_option($sock,IPPROTO_IP,MCAST_JOIN_GROUP, $grpparms);
 	socket_recvfrom($sock,$recvStr,$len,$flags,$from,$port);
-	echo("Received " . $recvStr . " from " . $from); 
+	echo("Received " . $recvStr . " from " . $from ."\n"); 
 	socket_close($sock);
 
 ?>
