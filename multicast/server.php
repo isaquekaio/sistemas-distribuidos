c1c7d8353e0109dc0716e2b207570ab7fd7d9228
Isaque Kaio <32231299+kaio7@users.noreply.github.com>
2019-04-29 13:31:52 -0300

    Consertando o codigó com a ajuda de Israel

--- multicast/server.php
+++ multicast/server.php
@@ -1,10 +1,10 @@
 <?php
 	$port = 14446;
-	$grpparms = array("group" => "224.0.0.0", "interface" => "enp0s31f6");
-	$mesg = "E ai galera";
+	$grpparms = array("group" => "225.0.0.0", "interface" => "enp0s31f6");
+	$mesg = "E ai galera do youtube";
 	$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 	socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1);
 	socket_set_option($sock, IPPROTO_IP, MCAST_JOIN_GROUP, $grpparms);
-	socket_sendto($sock, $mesg, strlen($mesg), 0, '10.25.1.245', $port);
+	socket_sendto($sock, $mesg, strlen($mesg), 0, '225.0.0.0', $port);
 	socket_close($sock); 
 ?>
